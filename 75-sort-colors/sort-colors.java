2dccd9f7143fbf534ba12b86caca3b6634d4cf48
abhay pratap singh <anshu935156@gmail.com>
2026-08-15 22:15:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (89.95%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,71 @@
+class Solution {
+    public void sortColors(int[] nums) {
+       //brute force approach make three new array store 0,1, and 2 and store them in new array 
+    //    int zero[]=new int[nums.length];
+    //    int one[]=new int[nums.length];
+    //    int two[]=new int[nums.length];
+    //    int result[]=new int[nums.length];
+    //    int o=0;
+    //    int t=0;
+    //    int z=0;
+    //   for(int i=0;i<nums.length;i++){
+    //     if(nums[i]==0){
+    //         zero[z]=nums[i];
+    //         z++;
+    //     }else if(nums[i]==1){
+    //         one[o]=nums[i];
+    //         o++;
+    //     }else{
+    //         two[t]=nums[i];
+    //         t++;
+    //     }
+    //   }
+    //   int index=0;
+    //   for(int i=0;i<z;i++){
+    //     nums[index]=zero[i];
+    //     index++;
+    //   }
+    //   for(int i=0;i<o;i++){
+    //     nums[index]=one[i];
+    //     index++;
+    //   }
+    //   for(int i=0;i<t;i++){
+    //     nums[index]=two[i];
+    //     index++;
+    //   }
+ 
+ // second approach is the optimized brute force approach in which we one time traverse and count number of zero,one and two and then modify nums;
+ int z=0;
+ int o=0;
+ int t=0;
+ for(int i=0;i<nums.length;i++){
+    if(nums[i]==0){
+        z++;
+
+    }else if(nums[i]==1){
+        o++;
+    }else{
+        t++;
+    }
+ }
+ int index=0;
+ while(z>0){
+    nums[index]=0;
+    index++;
+    z--;
+ }
+  while(o>0){
+    nums[index]=1;
+    index++;
+    o--;
+ }
+ while(t>0){
+    nums[index]=2;
+    index++;
+    t--;
+ }
+
+
+     
+    }
+}
