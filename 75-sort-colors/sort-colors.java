82f72a06423e23706457d6ca9685064c121c9ceb
abhay pratap singh <anshu935156@gmail.com>
2026-08-16 12:37:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (43.02%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -35,37 +35,57 @@ class Solution {
     //   }
  
  // second approach is the optimized brute force approach in which we one time traverse and count number of zero,one and two and then modify nums;
- int z=0;
- int o=0;
- int t=0;
- for(int i=0;i<nums.length;i++){
-    if(nums[i]==0){
-        z++;
+//  int z=0;
+//  int o=0;
+//  int t=0;
+//  for(int i=0;i<nums.length;i++){
+//     if(nums[i]==0){
+//         z++;
 
-    }else if(nums[i]==1){
-        o++;
+//     }else if(nums[i]==1){
+//         o++;
+//     }else{
+//         t++;
+//     }
+//  }
+//  int index=0;
+//  while(z>0){
+//     nums[index]=0;
+//     index++;
+//     z--;
+//  }
+//   while(o>0){
+//     nums[index]=1;
+//     index++;
+//     o--;
+//  }
+//  while(t>0){
+//     nums[index]=2;
+//     index++;
+//     t--;
+//  }
+int mid=0;
+int high=nums.length-1;
+int low=0;
+while(mid<=high){
+    if(nums[mid]==0){
+        swap(nums,low,mid);
+        low++;
+        mid++;
+    }else if(nums[mid]==1){
+        mid++;
     }else{
-        t++;
+        swap(nums,high,mid);
+        high--;
     }
- }
- int index=0;
- while(z>0){
-    nums[index]=0;
-    index++;
-    z--;
- }
-  while(o>0){
-    nums[index]=1;
-    index++;
-    o--;
- }
- while(t>0){
-    nums[index]=2;
-    index++;
-    t--;
- }
+}
 
 
      
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
